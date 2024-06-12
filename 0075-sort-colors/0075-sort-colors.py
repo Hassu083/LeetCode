3d68089f866138c922ce30226e22eb799dd54b08
Hasnain Ahmed <122469073+Hassu083@users.noreply.github.com>
2024-06-12 18:48:40 +0500

    Time: 38 ms (51.67%), Space: 16.5 MB (79.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -8,11 +8,11 @@ class Solution:
         while i<=two:
             if nums[i]==0:
                 nums[i], nums[zero] = nums[zero], nums[i]
-                while len(nums)>zero and nums[zero] == 0:
-                    zero += 1
-                i = zero
+                zero += 1
+                i += 1
             elif nums[i] == 2:
                 nums[i], nums[two] = nums[two], nums[i]
-                two -= 1
+                while two >= i and nums[two]==2:
+                    two -= 1
             else:
                 i += 1
