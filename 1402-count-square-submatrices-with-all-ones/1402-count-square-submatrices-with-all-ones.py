3c2b3787faf0850549c0df48ae34ce2edece5d38
Hasnain Ahmed <122469073+Hassu083@users.noreply.github.com>
2024-10-27 09:53:48 +0500

    Time: 71 ms (40.58%), Space: 19.2 MB (20.45%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -9,8 +9,8 @@ class Solution:
                     # print(i,j,matrix[i-1][j] if i-1>-1 else 0, matrix[i][j-1] if j-1>-1 else 0, matrix[i-1][j-1] if i-1>-1 and j-1>-1 else 0)
                     matrix[i][j] += min(matrix[i-1][j] if i-1>-1 else 0, matrix[i][j-1] if j-1>-1 else 0,  matrix[i-1][j-1] if i-1>-1 and j-1>-1 else 0)
                 ans += matrix[i][j]
-                print(ans)
-        print(matrix)
+                # print(ans)
+        # print(matrix)
         return ans
 
 
