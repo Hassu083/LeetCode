3a1cf2eacfe57334b2a6782867648a9a2825d2be
Hasnain Ahmed <122469073+Hassu083@users.noreply.github.com>
2025-01-25 14:15:30 +0500

    Time: 135 ms (99.31%), Space: 46.3 MB (86.11%) - LeetHub

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,41 +1,24 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        
-
         n = len(nums)
-        sorted_nums = sorted(nums)
+        ordered_nums = sorted(nums)
+        prev = ordered_nums[0]
+        num_to_group = {}
+        current_group = 0
+        group_start = [0]
 
-        # form windows
-        i = 0
-        windows = []
-        while i < n:
-            j = i
-            while j+1 < n and sorted_nums[j+1]-sorted_nums[j] <= limit:
-                j += 1
-            windows.append([sorted_nums[i],sorted_nums[j],i])
-            i = j + 1
-        
-        # search within windows
-        def searchInWindows(num):
-            left, right = 0, len(windows)-1
-            while left <= right:
-                mid = (left+right)>>1
-                window = windows[mid]
-                if window[0] <= num <= window[1]:
-                    return mid
-                elif window[0] > num:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            # if code returns -1 means some error need to be resolved
-            return -1
-        
-        # form result to return 
-        ans = []
-        for num in nums:
-            idx = searchInWindows(num)
-            index_sorted_nums = windows[idx][2]
-            windows[idx][2] = index_sorted_nums + 1
-            ans.append(sorted_nums[index_sorted_nums])
+        for i, x in enumerate(ordered_nums):
+            if x - prev > limit:
+                current_group += 1
+                group_start.append(i)
 
-        return ans
+            num_to_group[x] = current_group
+            prev = x
+            
+        result = []
+        for x in nums:
+            group = num_to_group[x]
+            result.append(ordered_nums[group_start[group]])
+            group_start[group] += 1
+
+        return result
